d1199fc7e50c148a30f8fd3b263c6f643cb1a7b5
tikt4096 <kl.9960@gmail.com>
2020-09-28 17:50:18 +0900

    関数エクスポート

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-function colorToString(color){
+export function colorToString(color){
 	if(color.r){
 		let r = zeroPad(color.r.toString(16),2);
 		let g = zeroPad(color.g.toString(16),2);
@@ -13,27 +13,27 @@ function colorToString(color){
 	}
 }
 
-function zeroPad(num,digit){
+export function zeroPad(num,digit){
 	return (Array(digit).join("0") + num).slice(-digit);
 }
 
-function setValue(element,value){
+export function setValue(element,value){
 	element.value = value;
 	if(element.type === "range"){
 		setNumber(element);
 	}
 }
 
-function setNumber(element){
+export function setNumber(element){
 	let num = document.getElementById(element.id + "_num");
 	num.textContent = element.value;
 }
 
-function setRange(element,min,max){
+export function setRange(element,min,max){
 	element.min = min;
 	element.max = max;
 }
 
-function rand(min,max){
+export function rand(min,max){
 	return Math.floor(Math.random() * (max + 1 - min)) + min;
 }
