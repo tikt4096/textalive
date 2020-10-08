78f2734637c8b33ed97a65d335323ede8470df72
tikt4096 <kl.9960@gmail.com>
2020-10-08 11:43:37 +0900

    文字の描画部分を変更。

--- character.js
+++ character.js
@@ -1,6 +1,11 @@
+import {Mosaic} from "./mosaic.js";
+//import * as func from "./function.js";
+
 //文字がフェードイン・アウトする時間(f)
 const FADE_IN_TIME = 30;
 const FADE_OUT_TIME = 30;
+
+const MOSAIC_SIZE = 4; //文字の分割サイズ(px)
 
 //文字の一文字の情報を保持
 export class Character{
@@ -17,6 +22,9 @@ export class Character{
 		this.size = 0;
 		this.easeFunc = ease;
 		this.ctx = ctx;
+		this.mosaic = new Mosaic(MOSAIC_SIZE,this.fontSize);
+		this.cMosaic = this.mosaic.makeCharMosaic(this.char);
+		this.pSize = Math.floor(this.fontSize / this.cMosaic.num);
 	}
 
 	update(){
@@ -37,8 +45,18 @@ export class Character{
 			ctx.globalAlpha = 1.0;
 		}
 
-		ctx.font = this.size + "px " + this.fontFamily;
-		ctx.fillText(this.char,this.x,this.y);
+		//文字を描画
+		let mosaic = this.cMosaic.mosaic;
+		for(let i = 0;i < mosaic.length;i++){
+			if(mosaic[i]){
+				let n = this.size / this.cMosaic.num;
+				let x = i % this.cMosaic.num * n - this.size / 2;
+				let y = Math.floor(i / this.cMosaic.num) * n - this.size / 2;
+				ctx.beginPath();
+				ctx.arc(this.x + x,this.y + y,this.pSize / 2,0,Math.PI * 2,false);
+				ctx.fill();
+			}
+		}
 		if(this.lifeTime <= 0){
 			this.isRemove = true;
 		}
